afbca0edffba1ba045dc945eed7e6e696b6e895d
portofalex <portofalex13@gmail.com>
2023-03-10 16:42:05 -0800

    Basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <div className="text-2xl text-red-200">
-      Hello World
+  return <div className="h-screen w-screen flex flex-col justify-center items-center">
+    <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+    <div className="p-2"></div>
+    <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+      <div className="w-16 h-16 bg-red-200"></div>
+      <div className="p-8">Vs</div>
+      <div className="w-16 h-16 bg-red-200"></div>
     </div>
-  )
+  </div>;
 }
