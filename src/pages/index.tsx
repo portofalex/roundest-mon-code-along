c2c3f58b5d8eb781919451ddfc70e2b949b8e5e2
portofalex <portofalex13@gmail.com>
2023-03-10 17:23:01 -0800

    Configure trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import { Inter } from 'next/font/google';
+import { trpc } from '../utils/trpc';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const { data } = trpc.hello.useQuery({ text: 'Momma' });
+
+  if (!data) return <div>Loading...</div>;
+
+  return <div>{data.greeting}</div>
+
   return <div className="h-screen w-screen flex flex-col justify-center items-center">
     <div className="text-2xl text-center">Which Pokemon is rounder?</div>
     <div className="p-2"></div>
