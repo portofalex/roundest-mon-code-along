ae5d9bce75a63917a32e27f3efa910affd7ff771
portofalex <portofalex13@gmail.com>
2023-03-11 17:13:05 -0800

    Add image host for pokemon sprites and ignore eslint for builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
